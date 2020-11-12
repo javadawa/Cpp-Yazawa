b9876d518ddd1a5e624433b808dfdee5a8c0d2b7
tako <javadava.58.pts@gmail.com>
2020-11-12 23:18:31 +0900

    switch文

---
+++ list_3-4b_switch.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int channel;
+    string fee;
+
+    // キー入力をchannelに格納する
+    cout << "テレビのチャンネル番号を入力して下さい：";
+    cin >> channel;
+
+    // テレビ居の番号をfeeに格納する
+    switch (channel)
+    {
+    case 1:
+    case 3:
+        fee = "有料です。";
+        break;
+    case 4:
+    case 6:
+    case 8:
+    case 10:
+    case 12:
+        fee = "無料です。";
+        break;
+    default:
+        fee = "チャンネルの割り当てがありません。";
+        break;
+    }
+
+    cout << fee << endl;
+
+    return 0;
+}
